876488fb1675e7351f8a2c2cab450bfeb1a0bf2e
Martin Daguerre <59453458+martin-daguerre-pyxis@users.noreply.github.com>
2023-10-03 17:26:05 -0300

    cambio texto home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,6 +3,7 @@ import publicUrl from '../utils';
 import Article from '../components/content/Article';
 import ItemListContainer from '../components/products/ItemListContainer';
 import withAuth from '../hocs/withAuth';
+import { Link } from "react-router-dom";
 
 const Home = (props) => {
   const context = {
@@ -13,12 +14,13 @@ const Home = (props) => {
     <DefaultLayout context={context} className="flex flex-col justify-around flex-grow layout_home-page font-body">
       <Article
         picture={publicUrl + "subscribe.svg"}
-        title="Bienvenid@!"
+        title="Encuentra las mejores ofertas en Accesorios deportivos!"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac interdum eros, in accumsan neque. Phasellus sollicitudin dapibus scelerisque.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac interdum eros, in accumsan neque. Phasellus sollicitudin dapibus scelerisque."
-        link="#"
+        link="/category/3/Accesorios%20deportivos"
         pictureClass="subscribe animated w-full sm:w-[400px]"
       >
         <span className="pt-2 text-lg transition-all duration-700 font-title line-clamp-4 hover:line-clamp-none">Donec ac interdum eros, in accumsan neque. Phasellus sollicitudin dapibus scelerisque.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+        <Link className="btn" to="/category/3/Accesorios%20deportivos">Accesorios deportivos</Link>
       </Article>
       <ItemListContainer context={context}></ItemListContainer>
     </DefaultLayout>
